9f7928d6cc2544e8aba284810b480f3a0a3e3416
PrincessLeia <jango007@gmx.de>
2014-12-13 17:37:57 +0100

    Upload

---
+++ Ultimate Carry Prevolution/Ultimate Carry Prevolution/Loader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using LeagueSharp;
+
+namespace Ultimate_Carry_Prevolution
+{
+	class Loader
+	{
+		public const string VersionNumber = "1.2";
+
+		public Loader()
+		{
+			Chat.WellCome();
+		}
+
+	
+	}
+}
